795c041b3ea1a66a96c049cc1b183861b670e26e
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2023-02-27 16:45:51 -0300

    database table schema

---
+++ src/config/tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "users" (
+    "id" SERIAL PRIMARY KEY,
+    "cpf" CHAR(14) UNIQUE NOT NULL,
+    "private" INTEGER NOT NULL,
+    "incompleto" INTEGER NOT NULL,
+    "dataUltimaCompra" DATE,
+    "ticketMedio" FLOAT,
+    "ticketUltimaCompra" FLOAT,
+    "lojaFrequente" CHAR(18),
+    "lojaUltimaCompra" CHAR(18)
+)
