6ffc71827aa19fac8941a277d6b544708fa66060
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2023-02-28 20:32:50 -0300

    refactor: Query time drastically reduced.

--- src/repositories/fileRepository.ts
+++ src/repositories/fileRepository.ts
@@ -1,16 +1,16 @@
+import format from "pg-format"
+
 import connection from "../config/database.js"
 
 export async function fillDatabase(completeData: string[][]) {
     try {
-        // Running the array into a loop and inserting one by one
-        // TODO: Find a quicker, more performatic way of bulk inserting
-        for (let i = 0; i < completeData.length; i++) {
-            const query = {
-                text: `INSERT INTO users (cpf, private, incompleto, "dataUltimaCompra", "ticketMedio", "ticketUltimaCompra", "lojaFrequente", "lojaUltimaCompra") VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
-                values: completeData[i],
-            }
-            await connection.query(query)
-        }
+        // Inserting all data at once via the format function [concatenating all strings at once and doing a single query]
+
+        const query = format(
+            'INSERT INTO users (cpf, private, incompleto, "dataUltimaCompra", "ticketMedio", "ticketUltimaCompra", "lojaFrequente", "lojaUltimaCompra") VALUES %L',
+            completeData
+        )
+        await connection.query(query)
     } catch (e) {
         console.error(e)
     }
